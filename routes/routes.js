cc4df49e83b149e2085dea89c158520459a5ac5a
Rafael <macedo.rafanazareth@gmail.com>
2020-05-19 00:42:26 -0300

    Fixing

--- routes/routes.js
+++ routes/routes.js
@@ -3,6 +3,10 @@ const routes = express.Router();
 const FakeEstufa = require('../console/mockSchedule');
 
 routes.get('/', (req, res) => {
+    if(req.query == undefined){
+        return res.send("Send params")
+    }
+    
     var on = req.query.id
     if(on == 1){
         FakeEstufa.start();
